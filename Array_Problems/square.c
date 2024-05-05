c3667d8cd6e59cf9a55569806017aa3de3b72f99
Dan Njuguna <126158747+Dan-njuguna@users.noreply.github.com>
2024-05-05 10:33:31 +0300

    Update square.c

--- Array_Problems/square.c
+++ Array_Problems/square.c
@@ -8,33 +8,33 @@
 int main(void)
 {
 	int N; /*Number of points*/
-    int i;
-    float maxdist, sidelength;
+	int i;
+	float maxdist, sidelength;
 
-    scanf("%d", &N);
-    if (N < 2 || N > 1000)
-    {
-        exit(EXIT_FAILURE);
-    }
-    point *Points = (point *)malloc(N * sizeof(point));
-    if (!Points)
-    {
-        exit(EXIT_FAILURE);
-    }
+    	scanf("%d", &N);
+    	if (N < 2 || N > 1000)
+    	{
+        	exit(EXIT_FAILURE);
+    	}
+    	point *Points = (point *)malloc(N * sizeof(point));
+    	if (!Points)
+    	{
+        	exit(EXIT_FAILURE);
+    	}
 
-    for (i = 0; i < N; i++)
-    {
-        scanf("%f %f", &Points[i].x, &Points[i].y);
-    }
+    	for (i = 0; i < N; i++)
+    	{
+        	scanf("%f %f", &Points[i].x, &Points[i].y);
+    	}
 
-    maxdist = findmaxdist(Points, N);
+	maxdist = findmaxdist(Points, N);
 
-    sidelength = maxdist / sqrt(2.0);
+    	sidelength = maxdist / sqrt(2.0);
 
-    printf("%.4f\n", sidelength);
-    free(Points);
+    	printf("%.4f\n", sidelength);
+    	free(Points);
 
-    return (0);
+    	return (0);
 }
 /**
  * calc_dist - calculates Euclidean distance between two points
